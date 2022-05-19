25c25fff5d95c7139eda3b7cc5b919c714df61f8
Andres Leotur <leoturorellana@gmail.com>
2022-05-18 20:30:21 -0400

    iniciando proyecto

---
+++ src/main/java/com/core/validatecredit/ValidateCreditApplication.java
@@ -0,0 +1,13 @@
+package com.core.validatecredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/validatecredit/ValidateCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.validatecredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
