4be08dfa0a595762aa13d89d8db99df801c24939
Andres Leotur <leoturorellana@gmail.com>
2022-05-18 23:46:53 -0400

    Realizando validación

--- src/main/java/com/core/validatecredit/controller/RequestController.java
+++ src/main/java/com/core/validatecredit/controller/RequestController.java
@@ -1,9 +1,8 @@
 package com.core.validatecredit.controller;
 
-import com.core.validatecredit.model.Request;
+
 import com.core.validatecredit.model.ResponseModel;
 import com.core.validatecredit.presenter.RequestPresenter;
-import com.core.validatecredit.service.RequestServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -19,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/")
 @RestController
 public class RequestController<T> {
-
 
 
     @Autowired
--- src/main/java/com/core/validatecredit/presenter/RequestPresenter.java
+++ src/main/java/com/core/validatecredit/presenter/RequestPresenter.java
@@ -4,20 +4,33 @@ import com.core.validatecredit.model.ResponseModel;
 import com.core.validatecredit.service.RequestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class RequestPresenter {
 
     @Autowired
     private RequestServiceImpl requestServiceImpl;
 
+    //Para probar el servicio
+    private final String DOCUMENT_USUARIO = "20044824";
+
     public ResponseModel<Boolean> idCredit() {
         ResponseModel<Boolean> responseModel = null;
-        final boolean IS_CREDIT = requestServiceImpl.isCredit("20044824").isEmpty();
-        if (IS_CREDIT)
-            responseModel = new ResponseModel<Boolean>("00", "No tiene créditos", IS_CREDIT);
-        else
-            responseModel = new ResponseModel<Boolean>("99", "Si tiene créditos ", IS_CREDIT);
+        final boolean IS_NO_REQUEST = requestServiceImpl.findRequest(DOCUMENT_USUARIO).isEmpty();
+        log.info("PRESENTER- {}", IS_NO_REQUEST);
+        if (IS_NO_REQUEST) {
+            responseModel = new ResponseModel<Boolean>("00", "El cliente No tiene solicitud", IS_NO_REQUEST);
+        } else {
+            log.info("INGRESANDO IS_REQUEST");
+            final boolean STATUS_CREDIT = requestServiceImpl.isCredit(DOCUMENT_USUARIO).get().equalsIgnoreCase("Pagado");
+            log.info(requestServiceImpl.isCredit(DOCUMENT_USUARIO).get());
+            if (STATUS_CREDIT)
+                responseModel = new ResponseModel<Boolean>("00", "Estatus crédito pagado", STATUS_CREDIT);
+            else
+                responseModel = new ResponseModel<Boolean>("99", "Tiene créidto activo", STATUS_CREDIT);
+        }
         return responseModel;
     }
 
