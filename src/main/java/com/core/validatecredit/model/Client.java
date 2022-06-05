a1a23c2be7a31d06d2c5d4938883f28c1b1939ad
Andres Leotur <leoturorellana@gmail.com>
2022-06-04 22:17:43 -0400

    ajustando los campos de la entidad Client

--- src/main/java/com/core/validatecredit/model/Client.java
+++ src/main/java/com/core/validatecredit/model/Client.java
@@ -1,5 +1,6 @@
 package com.core.validatecredit.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,12 +20,19 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_CLIENT")
     private Long id;
+    @Column(name = "TYEP_DOCUMENT")
     private String typeDocument;
+    @Column(name = "DOCUMENT")
     private int document;
+    @Column(name = "NAME")
     private String name;
+    @Column(name = "PHONE")
     private String phone;
+    @Column(name = "HOME_PHONE")
     private String homePhone;
+    @Column(name = "EXIST_PATRIA")
     private boolean existPatria;
+    @Column(name = "EXIST_EXCHANGE")
     private boolean existExchange;
 
 }
